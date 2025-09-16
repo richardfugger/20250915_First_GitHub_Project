dcb3951d4cfbf575db6f68dc5ac01f4093adabf3
richardfugger <richard.fugger@gmail.com>
2025-09-16 12:52:51 +0200

    funktioiniert

--- 20250915_First_GitHub_Project/Program.cs
+++ 20250915_First_GitHub_Project/Program.cs
@@ -1,16 +1,1 @@
-﻿Console.WriteLine("Version : #1");
-Console.WriteLine("Add_Something: #1");
-Console.WriteLine("Add_Something: #4");
-Console.WriteLine("Edit_Something: #5");
-
-Console.WriteLine("Make_Something_New #1");
-Console.WriteLine("Make_Something_New #2");
-
-Console.WriteLine("Next 1");
-Console.WriteLine("Next ^2");
-
-Console.WriteLine("NEXT A");
-Console.WriteLine("NEXT b");
-
-Console.WriteLine("Test");
-Console.WriteLine("tst");
+﻿Console.WriteLine("Jetzt funktioinert es!");
